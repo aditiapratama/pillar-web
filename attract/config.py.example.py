12729ba37b3b69735fde2e69c5aa759393235888
Eibriel <eibriel@yahoo.com.ar>
2015-04-22 07:13:32 -0300

    Adding missing parameters to config.py.example

--- attract/config.py.example.py
+++ attract/config.py.example.py
@@ -22,7 +22,10 @@ class Config(object):
 
 class Development(Config):
     SECRET_KEY=''
-    SERVER_NAME='attract.local:5555'
+    #SERVER_NAME='attract.local:5555'
+    ATTRACT_SERVER='http://127.0.0.1:5000'
+    HOST='0.0.0.0'
+    PORT=5001
     DEBUG=True
     SQLALCHEMY_DATABASE_URI=''
     SECURITY_REGISTERABLE=True
@@ -33,3 +36,4 @@ class Development(Config):
     MEDIA_THUMBNAIL_URL = ''
     FILE_STORAGE = '{0}/application/static/storage'.format(
                 os.path.join(os.path.dirname(__file__)))
+    FILE_STORAGE_BACKEND = 'fs.files'
