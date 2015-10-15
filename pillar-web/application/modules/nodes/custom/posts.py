dfaba7559cdee5e8397367b926a2d07521502690
Francesco Siddi <francesco.siddi@gmail.com>
2015-10-15 20:26:20 +0200

    Tweaks to post creation workflow

--- pillar-web/application/modules/nodes/custom/posts.py
+++ pillar-web/application/modules/nodes/custom/posts.py
@@ -3,6 +3,8 @@ from pillarsdk import NodeType
 from pillarsdk.exceptions import ResourceNotFound
 from flask import abort
 from flask import render_template
+from flask import redirect
+from flask import url_for
 from flask.ext.login import login_required
 from flask.ext.login import current_user
 from application import app
@@ -39,29 +41,34 @@ def posts_view(project_id, url=None):
         posts = Node.all({
             'where': '{"parent": "%s"}' % (blog._id),
             'embedded': '{"picture":1}',
+            'sort': '-_created'
             }, api=api)
         return render_template(
             'nodes/custom/blog/index.html',
             posts=posts._items)
 
 
-@nodes.route("/posts/<project>/create", methods=['GET', 'POST'])
+@nodes.route("/posts/<project_id>/create", methods=['GET', 'POST'])
 @login_required
-def posts_create(project):
+def posts_create(project_id):
     api = SystemUtility.attract_api()
+    node_type = NodeType.find_first({
+        'where': '{"name" : "blog"}',
+        'projection': '{"name": 1}'
+        }, api=api)
+    blog = Node.find_first({
+        'where': '{"node_type" : "%s", \
+            "parent": "%s"}' % (node_type._id, project_id),
+        }, api=api)
     node_type = NodeType.find_first({'where': '{"name" : "post"}',}, api=api)
     form = get_node_form(node_type)
     if form.validate_on_submit():
         user_id = current_user.objectid
         if process_node_form(
                 form, node_type=node_type, user=user_id):
-            return 'ok'
-        else:
-            error = "Server error"
-            print ("Error sending data")
-        return 'ok'
-
+            return redirect(url_for('main_blog'))
+    form.parent.data = blog._id
     return render_template('nodes/custom/post/create.html',
         node_type=node_type,
         form=form,
-        project=project)
+        project_id=project_id)
