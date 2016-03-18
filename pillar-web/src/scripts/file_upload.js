4be935daab46c3c9b28207e7b2d70284bfbea16a
Francesco Siddi <francesco.siddi@gmail.com>
2016-03-18 18:43:12 +0100

    Unified file upload js

---
+++ pillar-web/src/scripts/file_upload.js
@@ -0,0 +1,78 @@
+function deleteFile(fileField, newFileId) {
+    newFileId = newFileId || null;
+    var fileId = fileField.val();
+    var url = '/files/delete/' + fileId;
+    $.post(url, function(data){}).success(function(dataResponse){
+        // Notify of successful request
+        //- statusBarSet('success', 'Original File Removed Successfully');
+        // If we provide the id of a previously uploaded file, add it here
+        if (newFileId) {
+            fileField.val(newFileId);
+        } else {
+            fileField.val('');
+        }
+    });
+}
+
+$('.file_delete').click(function(e){
+    e.preventDefault();
+    var field_name = '#' + $(this).data('field-name');
+    var file_field = $(field_name);
+    deleteFile(file_field);
+    $('.node-preview-thumbnail').hide();
+});
+
+
+$(function () {
+    $('.fileupload').fileupload({
+        dataType: 'json',
+        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+        replaceFileInput:false,
+        dropZone: $(this),
+        formData: {},
+        progressall: function (e, data) {
+          // Update progressbar during upload
+					var progress = parseInt(data.loaded / data.total * 100, 10);
+          $(this).next().find('.picture-progress-bar').css(
+						{'width': progress + '%', 'display': 'block'}
+					).addClass('progress-active');
+        },
+        done: function (e, data) {
+            // Get the first file upload result (we only need one)
+            var fileData = data.result.files[0];
+            // Create a file object on the server and retrieve its id
+            statusBarSet('info', 'Uploading File');
+
+            $('.button-save').addClass('disabled');
+            $('li.button-save a#item_save').html('<i class="pi-spin spin"></i> Uploading Preview');
+
+            var payload = {
+                name: fileData.name,
+                size: fileData.size,
+                type: fileData.type,
+                field_name: $(this).data('field-name'),
+                project_id: ProjectUtils.projectId()
+            }
+            $.post("/files/create", payload)
+                .done(function(data) {
+                if (data.status === 'success') {
+                    // If successful, add id to the picture hidden field
+                    var field_name = '#' + data.data.field_name;
+                    if ($(field_name).val()){
+                        $('.node-preview-thumbnail').hide();
+                        deleteFile($(field_name), data.data.id);
+                    } else {
+                        $(field_name).val(data.data.id);
+                    }
+                    $('.node-preview-thumbnail').attr('src', data.data.link);
+                    $('.node-preview-thumbnail').show();
+                    statusBarSet('success', 'File Uploaded Successfully');
+
+                    $('.button-save').removeClass('disabled');
+                    $('li.button-save a#item_save').html('<i class="pi-check"></i> Save Changes');
+									$('.progress-active').hide().removeClass('progress-active');
+                }
+            });
+        }
+    });
+});
--- pillar-web/src/scripts/tutti/0_navbar.js
+++ pillar-web/src/scripts/tutti/0_navbar.js
@@ -18,3 +18,43 @@ function NavbarTransparent() {
 
 NavbarTransparent();
 
+
+/* Status Bar */
+function statusBarSet(classes, html, icon_name){
+	/* Utility to notify the user by temporarily flashing text on the project header
+		 Usage:
+			'classes' can be: success, error, warning, info, default
+			'html': the text to display, can contain html tags
+				(in case of errors, it's better to use data.status + data.statusText instead )
+			'icon_name': optional, sets a custom icon (otherwise an icon based on the class will be used)
+	*/
+
+	var icon = '';
+
+	if (!icon_name) {
+		if (classes == 'error') {
+			icon_name = 'ion-alert-circled';
+		} else if (classes == 'success') {
+			icon_name = 'ion-checkmark-round';
+		} else if (classes == 'warning') {
+			icon_name = 'ion-alert-circled';
+		} else if (classes == 'info') {
+			icon_name = 'ion-information-circled';
+		} else {
+			icon = '<i class="' + icon_name + '"></i>';
+		};
+	} else {
+		icon = '<i class="' + icon_name + '"></i>';
+	};
+
+	var text = icon + html;
+	$("#project-statusbar").addClass('active ' + classes);
+	$("#project-statusbar").html(text);
+
+	/* Back to normal */
+	setTimeout(function(){
+	$("#project-statusbar").removeAttr('class');
+	$("#project-statusbar").html();
+	}, 2000);
+};
+
